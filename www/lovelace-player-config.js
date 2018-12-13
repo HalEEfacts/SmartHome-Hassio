93e1922ccdc226b1c95d389725bd10d4361c2b58
HalEEfacts <trev.macinnis@gmail.com>
2018-12-13 09:23:48 -0400

    first commit

---
+++ www/lovelace-player-config.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+
+  // Add your bindings here
+  // Example:
+  LovelacePlayer.bind("media_player.livingroom_dashboard", "37ad9100-b972298c");
+
+  // Remove this line when you found your Device ID
+  //LovelacePlayer.debug();
+}, 200);
